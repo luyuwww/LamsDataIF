1d4943d5ad6828db023aa000f1135f26ad3a4767
zzwcl <zzwcl@163.com>
2018-03-27 18:00:29 +0800

    ttt

--- src/test/java/com/bwzk/junit/TestJsonBean.java
+++ src/test/java/com/bwzk/junit/TestJsonBean.java
@@ -3,8 +3,10 @@ package com.bwzk.junit;/**
  */
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.bwzk.pojo.jsonbean.AddItem;
 import com.bwzk.pojo.jsonbean.DelItem;
+
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
@@ -20,34 +22,40 @@ public class TestJsonBean {
 
     @Test
     public void json2Bean() throws IOException {
-        File del = new File("D:\\workspace\\idea\\LamsDataIF\\docu\\del2Da_single.json");
-        File add = new File("D:\\workspace\\idea\\LamsDataIF\\docu\\add2Da_single.json");
+      //  File del = new File("D:\\workspace\\idea\\LamsDataIF\\docu\\del2Da_single.json");
+         File add = new File("D:\\data\\add2Da_single.json");
         String addStr = FileUtils.readFileToString(add);
-        String delStr = FileUtils.readFileToString(del);
-        AddItem addItem =JSON.parseObject(addStr, AddItem.class);
-        //System.out.println(addItem.getITEM().getEFILENAME());
+        //String delStr = FileUtils.readFileToString(del);
+        JSONObject jsStr = JSONObject.parseObject(addStr);
+        JSONObject jsStr1=jsStr.getJSONObject("messageBody");
+        JSONObject jsStr2=jsStr1.getJSONObject("data");
+        JSONObject jsStr3=jsStr2.getJSONObject("item");
+        System.out.println(jsStr3.toJSONString());
+        
+        AddItem addItem =JSON.parseObject(jsStr3.toJSONString(), AddItem.class);
+        System.out.println(addItem.getITEM().toString());
 
-        DelItem di = JSON.parseObject(delStr , DelItem.class);
+        //DelItem di = JSON.parseObject(delStr , DelItem.class);
         //System.out.println(di.getDELITEM().getEFILENAME());
 
 
     }
 
 
-    @Test
-    public void bean2Json() throws IOException {
-        AddItem addItem = new AddItem("2", "3",    "custid",    "CUSTCARD",    "CUSTNAME",
-                "FILETYPE",   "/sd/f/sdf/s/dfsd",   "sdfsdfdsf",  "doc",  "data" ,
-                1 , "sdfsdfmsmdfMD5" , "zxy" , "2018年3月22日17:12:47" ,12515161);
-
-        String add = JSON.toJSONString(addItem);
-        System.out.println(add);
-
-        DelItem delItem = new DelItem("2","21","/s/d/f/e/f/sx/dv/w/er" , "zxy","2018-3-22 17:17:22");
-        System.out.println(JSON.toJSONString(delItem));
-
-
-
-
-    }
+//    @Test
+//    public void bean2Json() throws IOException {
+//        AddItem addItem = new AddItem("2", "3",    "custid",    "CUSTCARD",    "CUSTNAME",
+//                "FILETYPE",   "/sd/f/sdf/s/dfsd",   "sdfsdfdsf",  "doc",  "data" ,
+//                1 , "sdfsdfmsmdfMD5" , "zxy" , "2018年3月22日17:12:47" ,12515161);
+//
+//        String add = JSON.toJSONString(addItem);
+//        System.out.println(add);
+//
+//        DelItem delItem = new DelItem("2","21","/s/d/f/e/f/sx/dv/w/er" , "zxy","2018-3-22 17:17:22");
+//        System.out.println(JSON.toJSONString(delItem));
+//
+//
+//
+//
+//    }
 }
